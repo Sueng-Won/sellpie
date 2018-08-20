ba3223643906afe1cd3c9a1772dde00769747f4b
gk___ <gk___@LAPTOP-FA2N9IT0>
2018-08-20 16:54:40 +0900

    ////////

--- sellpie/src/main/java/com/teamnameled/sellpie/resource/model/dao/ResourceDao.java
+++ sellpie/src/main/java/com/teamnameled/sellpie/resource/model/dao/ResourceDao.java
@@ -20,8 +20,8 @@ public class ResourceDao {
 		return sqlSession.insert("resourceMapper.insertResource", resource);
 	}
 
-	public int deleteResource(String oldFilePath) {
-		return sqlSession.delete("resourceMapper.deleteResource", oldFilePath);
+	public int deleteResource(String delfile) {
+		return sqlSession.delete("resourceMapper.deleteResource", delfile);
 	}
 
 
